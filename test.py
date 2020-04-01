d86e8801af4c761d5237ed59aee8e9d2767145cc
Claudiu <yatsukoyuno@gmail.com>
2020-04-02 01:21:29 +0300

    Writing output to file

--- test.py
+++ test.py
@@ -11,12 +11,14 @@ def read_input(dicom_file_path, seg_file_path):
 
     return array, seg
 
+
 def test_input(test_dir):
-        layer, seg = read_input(test_dir + 'in.in', test_dir + 'seg.in')
+    layer, seg = read_input(test_dir + 'in.in', test_dir + 'seg.in')
 
-        mask = extract_mask(layer, seg, sensitivity=0.7, ksize=(4,4), debug=True)
+    mask = extract_mask(layer, seg, sensitivity=0.7, ksize=(4, 4), debug=True)
 
-        overlay_mask(test_dir + 'dicom.png', mask)
+    write_mask(test_dir + 'my_seg.out', mask)
+    overlay_mask(test_dir + 'dicom.png', mask)
 
 
 if __name__ == '__main__':
